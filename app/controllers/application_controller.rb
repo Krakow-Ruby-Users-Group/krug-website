956d168be7888e30c580f06ccad192c877bd8204
gajewsky <marcin.gajewski@rebased.pl>
2016-09-17 20:31:49 +0200

    footer and next meeting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@
 # an action controller
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :next_meeting
   helper Bootsy::Engine.helpers
+
+  private
+
+  def next_meeting
+    @next_meeting = Event.last
+  end
 end
