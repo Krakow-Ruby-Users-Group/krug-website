a7162ab8fef7e8f910e8abd8bd3fca5b467be2ee
gajewsky <marcin.gajewski@rebased.pl>
2016-10-07 15:26:29 +0200

    Use event status to determine if it is past or upcoming

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@
 # an action controller
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :next_meeting
+  before_action :newest_meeting
   helper Bootsy::Engine.helpers
 
   private
 
-  def next_meeting
-    @next_meeting = Event.last
+  def newest_meeting
+    @newest_meeting = Event.last
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
   def active_class(link_path)
     current_page?(link_path) ? 'active' : ''
   end
+
+  def newest_meeting_header(event)
+    status = event.status == 'past' ? 'LAST' : 'NEXT'
+
+    "#{status} MEETING"
+  end
 end
