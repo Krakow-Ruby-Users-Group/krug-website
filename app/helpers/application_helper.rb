7bdd399ce201fd03678c21ed4674649fa2c626b4
gajewsky <marcin.gajewski@rebased.pl>
2016-10-31 23:34:34 +0100

    Newest meeting header, pagination fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,4 @@ module ApplicationHelper
   def active_class(link_path)
     current_page?(link_path) ? 'active' : ''
   end
-
-  # Prepare newest event header
-  def newest_meeting_header(event)
-    status = event.status == 'past' ? 'LAST' : 'NEXT'
-    "#{status} MEETING"
-  end
 end
