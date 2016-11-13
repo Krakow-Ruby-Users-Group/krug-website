f88d461cef968f24af2c87c225532b28baa4a1a0
Maciej Mensfeld <maciej@mensfeld.pl>
2016-11-13 11:28:21 +0100

    sponsors order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@
 class PagesController < ApplicationController
   # Setup and trigger landing page
   def index
-    @sponsors = Sponsor.all
+    @sponsors = Sponsor.sorted
   end
 end
--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,4 +1,6 @@
 # Model representing single KRUG sponsor
 class Sponsor < ApplicationRecord
   mount_uploader :logo, LogoUploader
+
+  scope :sorted, -> { order(created_at: :desc) }
 end
