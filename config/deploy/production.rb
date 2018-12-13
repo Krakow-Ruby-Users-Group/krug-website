fd1726d77277198952ca748e0a2f1f69a7957568
Maciej Mensfeld <maciej@mensfeld.pl>
2018-12-13 09:39:40 +0100

    newer ruby

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,8 @@
 server 'ebisu', user: 'mensfeld', roles: %w{web app db}
 set :stage, :production
 
+set :ssh_options, forward_agent: true
+
 set :linked_files, %w{
   config/secrets.yml
   config/database.yml
